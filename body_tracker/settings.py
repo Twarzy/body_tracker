e2598ed0b609ae0d3d72340986aea27abbd52803
Bartosz Kowalik <67926973+Twarzy@users.noreply.github.com>
2023-03-20 18:02:04 +0100

    Added render.com to allowed_host

--- body_tracker/settings.py
+++ body_tracker/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
-                 'body-tracker.herokuapp.com'
+                 'body-tracker.onrender.com'
                  ]
 
 # Application definition
